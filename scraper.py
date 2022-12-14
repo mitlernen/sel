4f0983d44d0fc41f4ffd86c8959e8ee532579787
mitlernen <107308659+mitlernen@users.noreply.github.com>
2022-12-14 20:36:35 +0100

    with secret send the scraping now

--- scraper.py
+++ scraper.py
@@ -164,3 +164,48 @@ asdf
 
 url = f"https://api.telegram.org/bot{tmtoken}/sendMessage?chat_id={user}&text={asdf}"
 requests.get(url).json()
+
+
+import datetime as dt
+import os
+
+url = os.environ['MYURL']
+secretcookie = os.environ['SECRETCOOKIE']
+secretmiddlecookie = os.environ['SECRETMIDDLECOOKIE']
+my = os.environ['PASS']
+
+msg = asdf+ "\n" + str(dt.datetime.now())
+
+cookies = {
+'csrftoken': secretcookie,
+}
+
+headers = {
+'authority': 'rentry.co',
+'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.9',
+'accept-language': 'en-US,en;q=0.9',
+'cache-control': 'max-age=0',
+'cookie': 'csrftoken='+secretcookie,
+'origin': 'https://rentry.co',
+'referer': url,
+'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+'sec-ch-ua-mobile': '?0',
+'sec-ch-ua-platform': '"Linux"',
+'sec-fetch-dest': 'document',
+'sec-fetch-mode': 'navigate',
+'sec-fetch-site': 'same-origin',
+'sec-fetch-user': '?1',
+'upgrade-insecure-requests': '1',
+'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+}
+
+data = {
+'csrfmiddlewaretoken': secretmiddlecookie,
+'text': msg,
+'edit_code': my,
+'new_edit_code': '',
+'new_url': '',
+}
+
+response = requests.post(url, cookies=cookies, headers=headers, data=data)
+print(response)
